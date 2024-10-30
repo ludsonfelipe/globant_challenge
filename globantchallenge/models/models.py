4d9c2ab47101a6db078ffa32e6851914eba0ccd4
ludsonfelipe <felipe_ludson@hotmail.com>
2024-10-30 01:46:33 -0300

    (feat) creating db config e ORM models

---
+++ globantchallenge/models/models.py
@@ -0,0 +1,23 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from globantchallenge.core.database import Base
+
+
+class Department(Base):
+    __tablename__ = "departments"
+    id = Column(Integer, primary_key=True, index=True)
+    department = Column(String, index=True)
+
+
+class Job(Base):
+    __tablename__ = "jobs"
+    id = Column(Integer, primary_key=True, index=True)
+    job = Column(String, index=True)
+
+
+class HiredEmployee(Base):
+    __tablename__ = "hired_employees"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    datetime = Column(DateTime)
+    department_id = Column(Integer)
+    job_id = Column(Integer)
